6acc5f0f54365bb55bbab5deb92db48f9914ab73
OnceDeveloper <vkfks4607@gmail.com>
2021-07-02 15:45:52 +0900

    20201-07-02 modify <reducer> - overwrite id

--- redux/redux-saga-sample/src/store/posts/reducer.js
+++ redux/redux-saga-sample/src/store/posts/reducer.js
@@ -38,10 +38,14 @@ const PostReducer = (state = initialState, action) => {
       };
     case ADD_POST_SUCCESS:
       //console.log(action); => {type:"posts/ADD_...", payload :{id:101, title: '받은title값', body:'받은body값'}}
+      let lastValueId = 0;
+      if (state.posts.length !== 0) {
+        lastValueId = state.posts[state.posts.length - 1].id;
+      }
       return {
         ...state,
         isLoading: false,
-        posts: [...state.posts, action.payload],
+        posts: state.posts.concat({ ...action.payload, id: lastValueId + 1 }),
       };
     case ADD_POST_FAILED:
       return {
