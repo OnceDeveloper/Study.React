d9e6a7ba8d389fb67777bf6e5d21a924bcfcf16b
OnceDeveloper <vkfks4607@gmail.com>
2021-07-09 21:51:39 +0900

    firebase google login API  to card-maker project

--- card-maker/src/components/login/login.jsx
+++ card-maker/src/components/login/login.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LoginFooter from './loginFooter';
 import LoginHeader from './loginHeader';
 import styles from './login.module.css';
+import * as fire from '../../utils/firebase/firebase';
 
 const Login = (props) => {
+    // const onLoginGoogleApi = () => {
+    //     fire.signInWithGoogle
+    // }
+    const [curUser, setCurUser] = useState();
+    useEffect(() => {
+        fire.auth.onAuthStateChanged(user => {
+            setCurUser(user)
+        });
+        // console.log(curUser);
+
+    }, [curUser]);
+
     return (
         <div className={styles.loginContainer}>
             <LoginHeader />
             <div className={styles.loginArea}>
                 <div className={styles.loginTitle}> Login </div>
-                <button className={styles.loginButton}>
+                <button className={styles.loginButton} onClick={fire.signInWithGoogle}>
                     Google
                 </button>
                 <button className={styles.loginButton}>
--- card-maker/src/utils/firebase/firebase.js
+++ card-maker/src/utils/firebase/firebase.js
@@ -2,3 +2,37 @@ import firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
+
+// import * as firebaseui from 'firebaseui'
+// import 'firebaseui/dist/firebaseui.css'
+
+// var firebase = require('firebase');
+// var firebaseui = require('firebaseui');
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+// GoogleAuthProvider 클래스를 authentication 라이브러리에서 사용할 수 있도록 불러오는 코드.
+provider.setCustomParameters({ prompt: 'select_account' });
+// signIn이랑 authentication을 위해서 GoogleAuthProvider를 사용할 때마다 구글 팝업을 항상 띄우기를 원한다는 의미이다.
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+// signInWithPopup 메소드는 여러 파라미터를 받을 수 있다. 트위터, 페이스북, 깃허브 등으로도 로그인이 필요할 수도 있으므로.
+// 여기에서는 google로 signIn할 것이기 때문에, 파라미터로 위에서 정의한 provider를 넣어준다.
+
+export default firebase;
+// 혹시 전체 라이브러리가 필요할지도 모르기 때문에 firebase도 export 해준다.
+
+//firebase.analytics();
