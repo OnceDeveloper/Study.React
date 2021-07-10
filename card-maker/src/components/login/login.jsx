d7b5b8ff4a047a58e8e96dd60a040826ca7866ea
OnceDeveloper <vkfks4607@gmail.com>
2021-07-10 14:01:06 +0900

    google login debugging... to card-maker project

--- card-maker/src/components/login/login.jsx
+++ card-maker/src/components/login/login.jsx
@@ -3,26 +3,33 @@ import LoginFooter from './loginFooter';
 import LoginHeader from './loginHeader';
 import styles from './login.module.css';
 import * as fire from '../../utils/firebase/firebase';
-
+import firebase from 'firebase';
 const Login = (props) => {
     // const onLoginGoogleApi = () => {
     //     fire.signInWithGoogle
     // }
     const [curUser, setCurUser] = useState();
     useEffect(() => {
-        fire.auth.onAuthStateChanged(user => {
-            setCurUser(user)
-        });
+        // fire.auth.onAuthStateChanged(user => {
+        //     setCurUser(user)
+        // });
         // console.log(curUser);
-
-    }, [curUser]);
+        //firebase.auth().signInWithRedirect(fire.provider);
+    }, []);
+    const googleLogin = () => {
+        // debugger
+        // const provider = fire.googleProvider();
+        // firebase.auth().signInWithRedirect(provider);
+        // firebase.auth().getRedirectResult();
+    }
 
     return (
         <div className={styles.loginContainer}>
             <LoginHeader />
             <div className={styles.loginArea}>
                 <div className={styles.loginTitle}> Login </div>
-                <button className={styles.loginButton} onClick={fire.signInWithGoogle}>
+                <button className={styles.loginButton} onClick={googleLogin} >
+                    {/* onClick={fire.signInWithGoogle} */}
                     Google
                 </button>
                 <button className={styles.loginButton}>
